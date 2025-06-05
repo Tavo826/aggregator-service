d3cca8e322af5b1d6e1fdbc8a29bc76d59c3c847
Gustavo Orozco <gustavo.orozcom68@gmail.com>
2025-06-05 16:33:41 -0500

    request validator

--- src/main/java/com/trading/aggregator/exceptions/ApplicationExceptions.java
+++ src/main/java/com/trading/aggregator/exceptions/ApplicationExceptions.java
@@ -8,11 +8,19 @@ public class ApplicationExceptions {
         return Mono.error(new CustomerNotFoundException(customerId));
     }
 
-    public static <T> Mono<T> insufficientBalance(Integer customerId) {
-        return Mono.error(new InsufficientBalanceException(customerId));
+    public static <T> Mono<T> invalidTradeRequest(String message) {
+        return Mono.error(new InvalidTradeRequestException(message));
     }
 
-    public static <T> Mono<T> insufficientShares(Integer customerId) {
-        return Mono.error(new InsufficientSharesException(customerId));
+    public static <T> Mono<T> missingTicker() {
+        return Mono.error(new InvalidTradeRequestException("Ticker is required"));
+    }
+
+    public static <T> Mono<T> missingTradeAction() {
+        return Mono.error(new InvalidTradeRequestException("Trade action is required"));
+    }
+
+    public static <T> Mono<T> invalidQuantity() {
+        return Mono.error(new InvalidTradeRequestException("Quantity should be > 0"));
     }
 }
---
+++ src/main/java/com/trading/aggregator/exceptions/InvalidTradeRequestException.java
@@ -0,0 +1,8 @@
+package com.trading.aggregator.exceptions;
+
+public class InvalidTradeRequestException extends RuntimeException {
+
+    public InvalidTradeRequestException(String message) {
+        super(message);
+    }
+}
