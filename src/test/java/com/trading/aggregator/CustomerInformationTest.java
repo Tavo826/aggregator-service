7a6623a69da06e9bb6a3e88b6c9a0dec1decadbb
Gustavo Orozco <gustavo.orozcom68@gmail.com>
2025-06-06 19:55:17 -0500

    Integration tests - customer information api test

--- src/test/java/com/trading/aggregator/CustomerInformationTest.java
+++ src/test/java/com/trading/aggregator/CustomerInformationTest.java
@@ -6,6 +6,8 @@ import org.mockserver.model.HttpResponse;
 import org.mockserver.model.MediaType;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.test.web.reactive.server.WebTestClient;
 
 import java.util.Objects;
 
@@ -16,19 +18,44 @@ public class CustomerInformationTest extends AbstractIntegrationTest{
     @Test
     public void customerInformation() {
 
-        var responseBody = this.resourceToString("customer-service/customer-information-200.json");
+        mockCustomerInformation("customer-service/customer-information-200.json", 200);
+
+        getCustomerInformation(HttpStatus.OK)
+                .jsonPath("$.id").isEqualTo(1)
+                .jsonPath("$.name").isEqualTo("Sam")
+                .jsonPath("$.balance").isEqualTo(10000)
+                .jsonPath("$.holdings").isNotEmpty();
+    }
+
+    @Test
+    public void customerNotFound() {
+
+        mockCustomerInformation("customer-service/customer-information-404.json", 404);
+
+        getCustomerInformation(HttpStatus.NOT_FOUND)
+                .jsonPath("$.detail").isEqualTo("Customer [id=1] not found")
+                .jsonPath("$.title").isNotEmpty();
+    }
+
+    private void mockCustomerInformation(String path, int responseCode) {
+
+        var responseBody = this.resourceToString(path);
 
         mockServerClient
                 .when(HttpRequest.request("/customers/1"))
                 .respond(
                         HttpResponse.response(responseBody)
-                                .withStatusCode(200)
+                                .withStatusCode(responseCode)
                                 .withContentType(MediaType.APPLICATION_JSON)
                 );
+    }
 
-        this.client.get()
+    private WebTestClient.BodyContentSpec getCustomerInformation(HttpStatus expectedStatus) {
+
+        return this.client.get()
                 .uri("/customers/{customerId}", 1)
                 .exchange()
+                .expectStatus().isEqualTo(expectedStatus)
                 .expectBody()
                 .consumeWith(e -> log.info("{}", new String(Objects.requireNonNull(e.getResponseBody()))));
     }
